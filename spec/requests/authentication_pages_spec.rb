8e1fbad3a0796e785772ce6aff904a7091de058e
Kaina <kaina3053@gmail.com>
2013-03-21 09:51:34 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -41,34 +41,35 @@ describe "Authentication" do
   
   describe "authorization" do
 
-      describe "for non-signed-in users" do
-        let(:user) { FactoryGirl.create(:user) }
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
 
-        describe "when attempting to visit a protected page" do
-          before do
-            visit edit_user_path(user)
-            fill_in "Email",    with: user.email
-            fill_in "Password", with: user.password
-            click_button "Sign in"
-          end
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
 
-          describe "after signing in" do
-            it "should render the desired protected page" do
+        describe "after signing in" do
+          it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
-        end
-      end
 
-      describe "in the Microposts controller" do
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              click_link "Sign in"
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"              
+            end
 
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { response.should redirect_to(signin_path) }
-        end
-
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { response.should redirect_to(signin_path) }
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name)
+            end
+          end
         end
       end
 
@@ -79,15 +80,51 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
 
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
 
-        describe "submitting to the update action" do
-          before { put user_path(user) }
-          specify { response.should redirect_to(signin_path) }
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
         end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+    end
+
+    describe "in the Microposts controller" do
+
+      describe "submitting to the create action" do
+        before { post microposts_path }
+        specify { response.should redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete micropost_path(FactoryGirl.create(:micropost)) }
+        specify { response.should redirect_to(signin_path) }
+      end
+    end
+
+    describe "in the Relationships controller" do
+
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        specify { response.should redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete relationship_path(1) }
+        specify { response.should redirect_to(signin_path) }          
       end
     end
 
